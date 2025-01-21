325450382fd1c083f56b099a970bd5caa0703f88
MoisieievVasya <vmoisieiev@kse.org.ua>
2025-01-21 23:02:44 +0200

    Completed unit-1 with optional task

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -24,19 +24,44 @@ object arithmetic:
       else -value
 
   def addition(left: Number, right: Number): Number =
-    require(left >= 0, "Left must be non-negative")
-    require(right >= 0, "Right must be non-negative")
 
-    ???
+    @tailrec
+    def additionRec(left: Number, right: Number): Number =
+      if !isNonNegative(left) then
+        additionRec(decrement(left), increment(right))
+
+      else if isZero(right) then
+        left
+
+      else
+        additionRec(increment(left), decrement(right))
+
+    additionRec(left, right)
 
   def multiplication(left: Number, right: Number): Number =
     require(left >= 0, "Left must be non-negative")
     require(right >= 0, "Right must be non-negative")
 
-    ???
+    @tailrec
+    def multiplicationRec(left: Number, right: Number, acc: Number): Number =
+      if !isNonNegative(left) then
+        multiplicationRec(left, decrement(right), addition(acc, left))
+
+      else if isZero(right) then
+        acc
+
+      else
+        multiplicationRec(left, increment(right), addition(acc, -left))
+
+    multiplicationRec(left, right, acc = 0)
 
   def power(base: Number, p: Number): Number =
     require(p >= 0, "Power must be non-negative")
     require(base != 0 || p != 0, "0^0 is undefined")
 
-    ???
+    @tailrec
+    def powerRec(base: Number, p: Number, acc: Number): Number =
+      if isZero(p) then acc
+      else powerRec(base, decrement(p), multiplication(acc, base))
+
+    powerRec(base, p, acc = 1)
