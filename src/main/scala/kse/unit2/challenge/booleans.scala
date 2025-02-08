949194b09bb9a57504e9e09ea037c74a3a3110bd
MoisieievVasya <vmoisieiev@kse.org.ua>
2025-02-08 20:26:27 +0200

    Completed unit-2

--- src/main/scala/kse/unit2/challenge/booleans.scala
+++ src/main/scala/kse/unit2/challenge/booleans.scala
@@ -7,41 +7,51 @@ object booleans:
   case object True
   case object False
 
-  type True  = True.type
-  type False = False.type
-  type Boolean
+  type True    = True.type
+  type False   = False.type
+  type Boolean = True | False
 
-  val negation: Boolean => Boolean = ???
+  val negation: Boolean => Boolean = { case True => False; case False => True }
 
-  val conjunction: (Boolean, => Boolean) => Boolean = ???
+  val conjunction: (Boolean, => Boolean) => Boolean = (left, right) =>
+    if left == False then False else (left, right) match { case (True, value) => value; case (False, _) => False }
 
-  val disjunction: (Boolean, => Boolean) => Boolean = ???
+  val disjunction: (Boolean, => Boolean) => Boolean = (left, right) =>
+    if left == True then True else (left, right) match { case (True, _) => True; case (False, value) => value }
 
-  val implication: (Boolean, => Boolean) => Boolean = ???
+  val implication: (Boolean, => Boolean) => Boolean = (left, right) =>
+    if left == False then True else (left, right) match { case (True, value) => value; case (False, _) => True }
 
-  val equivalence: (Boolean, => Boolean) => Boolean = ???
+  val equivalence: (Boolean, => Boolean) => Boolean = { case (True, value) => value; case (False, value) => negation(value) }
 
   extension (value: Boolean)
 
     @targetName("negation")
-    infix def unary_! : Boolean = ???
+    infix def unary_! : Boolean = negation(value)
 
     @targetName("conjunction")
-    infix def ∧(that: => Boolean): Boolean = ???
+    infix def ∧(that: => Boolean): Boolean = conjunction(value, that)
 
     @targetName("disjunction")
-    infix def ∨(that: => Boolean): Boolean = ???
+    infix def ∨(that: => Boolean): Boolean = disjunction(value, that)
 
     @targetName("implication")
-    infix def →(that: => Boolean): Boolean = ???
+    infix def →(that: => Boolean): Boolean = implication(value, that)
 
     @targetName("equivalence")
-    infix def ↔(that: => Boolean): Boolean = ???
+    infix def ↔(that: => Boolean): Boolean = equivalence(value, that)
 
-  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean = ???
+  def fold(operation: (Boolean, Boolean) => Boolean, unit: Boolean)(list: List[Boolean]): Boolean =
 
-  val conjunctionOfElements: List[Boolean] => Boolean = ???
-  val disjunctionOfElements: List[Boolean] => Boolean = ???
+    @tailrec
+    def foldRec(list: List[Boolean], acc: Boolean): Boolean = list match
+      case Nil          => acc
+      case head :: tail => foldRec(tail, operation(head, acc))
+
+    foldRec(list, unit)
+
+  val conjunctionOfElements: List[Boolean] => Boolean = fold((left, right) => left ∧ right, True)
+  val disjunctionOfElements: List[Boolean] => Boolean = fold((left, right) => left ∨ right, False)
 
   extension (booleans: List[Boolean])
     infix def conjunction: Boolean = conjunctionOfElements(booleans)
