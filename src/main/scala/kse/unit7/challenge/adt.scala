63dbfd0929f4e7995128a648ff884c64bb2a318a
Vasyl <145612678+MoisieievVasya@users.noreply.github.com>
2025-03-29 11:16:42 +0200

    Completed unit 7

--- src/main/scala/kse/unit7/challenge/adt.scala
+++ src/main/scala/kse/unit7/challenge/adt.scala
@@ -4,11 +4,21 @@ object adt:
 
   enum Try[+V]:
 
-    case DummyCase
+    case Success(value: V)             extends Try[V]
+    case Failure(exception: Throwable) extends Try[Nothing]
 
-    def flatMap[Q](f: V => Try[Q]): Try[Q] = ???
+    def flatMap[Q](f: V => Try[Q]): Try[Q] =
+      this match
+        case Success(v) => f(v)
+        case Failure(e) => Try.Failure(e)
 
-    def map[Q](f: V => Q): Option[Q] = ???
+    def map[Q](f: V => Q): Try[Q] =
+      this match
+        case Success(v) => Try.Success(f(v))
+        case Failure(e) => Try.Failure(e)
 
   object Try:
-    def apply[V](v: V): Try[V] = ???
+
+    def apply[V](v: V): Try[V] =
+      try Success(v)
+      catch case e: Throwable => Failure(e)
--- src/main/scala/kse/unit7/challenge/app.scala
+++ src/main/scala/kse/unit7/challenge/app.scala
@@ -3,13 +3,43 @@ package kse.unit7.challenge
 import kse.unit7.challenge.adt.*
 import kse.unit7.challenge.model.*
 import kse.unit7.challenge.services.*
+import kse.unit7.topic.model.ApiKey
 
 object app:
 
-  def getPostsViews(): Try[List[PostView]] = ???
+  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] =
+    for
+      userProfile <- services.getUserProfile(apiKey)
+      posts       <- services.getPosts(userProfile.userId)
+      postViews <- posts.map(getPostView).foldLeft[Try[List[PostView]]](Try.Success(List.empty)) { (acc, postView) =>
+        for
+          list     <- acc
+          postView <- postView
+        yield list :+ postView
+      }
+    yield postViews
 
-  def getPostsViewDesugared(): Try[List[PostView]] = ???
+  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] =
+    services.getUserProfile(apiKey).flatMap { userProfile =>
+      services.getPosts(userProfile.userId).flatMap { posts =>
+        posts.map(getPostViewDesugared).foldLeft[Try[List[PostView]]](Try.Success(List.empty)) { (acc, postView) =>
+          acc.flatMap { list => postView.map { postView => list :+ postView } }
+        }
+      }
+    }
 
-  def getPostView(post: Post): Try[PostView] = ???
+  def getPostView(post: Post): Try[PostView] =
+    for
+      comments <- services.getComments(post.postId)
+      likes    <- services.getLikes(post.postId)
+      shares   <- services.getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
 
-  def getPostViewDesugared(post: Post): Try[PostView] = ???
+  def getPostViewDesugared(post: Post): Try[PostView] =
+    services.getComments(post.postId).flatMap { comments =>
+      services.getLikes(post.postId).flatMap { likes =>
+        services.getShares(post.postId).flatMap { shares =>
+          Try.Success(PostView(post, comments, likes, shares))
+        }
+      }
+    }
